d9ab41050f6a8b5175941a12a4e891210982285b
tushardongre1990 <tushardongre.1990@gmail.com>
2023-12-31 20:35:15 +0530

    65.Updating state based on current state

--- Section 6/steps/src/App.js
+++ Section 6/steps/src/App.js
@@ -14,59 +14,70 @@ const App = () => {
   3. update
   */
   const [step, setStep] = useState(1); // step 1 : Create
+  const [isOpen, setIsOpen] = useState(true);
 
   // use returns an array whose 1st element is the default value of the state and 2nd element i the function which updates the default value
-
   function handlePrevious() {
     if (step > 1) {
       // setStep(step - 1); // step 3 : update
 
-      // when we update state based on current state use callback func
+      // when we update state based on current state, use callback func
       setStep((s) => s - 1);
     }
   }
   function handleNext() {
     if (step < 3) {
       // setStep(step + 1); // step 3 : update
-
-      // when we update state based on current state use callback func
+      // when we update state based on current state, use callback func
       setStep((s) => s + 1);
     }
   }
 
+  function handleClose() {
+    // setIsOpen(!isOpen);
+    setIsOpen((is) => !is);
+  }
+
   return (
-    <div className="steps">
-      {/* step 2 : Use */}
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
-      <p className="message">
-        Step {step} : {messages[step - 1]}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-          // onClick={() => handleNext()} // can also written this way, but is redundant
-        >
-          Next
-        </button>
-        {/* <button
+    <>
+      <button className="close" onClick={handleClose}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          {/* step 2 : Use */}
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+          <p className="message">
+            Step {step} : {messages[step - 1]}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+              // onClick={() => handleNext()} // can also written this way, but is redundant
+            >
+              Next
+            </button>
+            {/* <button
           style={{ backgroundColor: "#7950f2", color: "#fff" }}
           onClick={() => alert("Next")} // don't directly call the alert func, call callback func i.e callback func is called when button is clicked
         >
           Next
         </button> */}
-      </div>
-    </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
