fb6fd95f0aff601755ca96ea6e51746a9701398c
tushardongre1990 <tushardongre.1990@gmail.com>
2024-01-02 01:05:36 +0530

    71.Rendering list items

--- Section 6/far-away/src/App.js
+++ Section 6/far-away/src/App.js
@@ -1,4 +1,8 @@
-import "./App.css";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 3, description: "Charger", quantity: 1, packed: true },
+];
 
 function App() {
   return (
@@ -24,7 +28,26 @@ const Form = () => {
 };
 
 const PackingList = () => {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const Item = ({ item }) => {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 };
 
 const Stats = () => {
