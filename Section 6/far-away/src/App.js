af839c937731cac47bf9173d32981d0a91e5bdd8
tushardongre1990 <tushardongre.1990@gmail.com>
2024-01-05 01:27:08 +0530

    85. Calculating Statistics as Derived State

--- Section 6/far-away/src/App.js
+++ Section 6/far-away/src/App.js
@@ -24,7 +24,9 @@ function App() {
 
   function handleToggleItem(id) {
     setItems((items) =>
-      items.map((item) => (item.id === id ? { ...item, packed: true } : item))
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
     );
   }
   return (
@@ -36,7 +38,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -141,10 +143,27 @@ const Item = ({ item, onDeleteItem, onToggleItem }) => {
   );
 };
 
-const Stats = () => {
+const Stats = ({ items }) => {
+  if (!items.length) {
+    return (
+      <p className="stats">
+        <em>Start adding some items to your list 🚀 .</em>
+      </p>
+    );
+  }
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percent = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>💼You have X items in your list. And you have already packed (X%)</em>
+      {percent === 100 ? (
+        <em>You got everything. Ready to go !!</em>
+      ) : (
+        <em>
+          💼You have {numItems} items in your list. And you have already packed
+          ({percent}%)
+        </em>
+      )}
     </footer>
   );
 };
