672fd182bb489fab557797f546ad336e7152f861
tushardongre1990 <tushardongre.1990@gmail.com>
2024-01-14 18:41:07 +0530

    119. Improving Reusability With Props

--- Section 10/use-popcorn/src/index.js
+++ Section 10/use-popcorn/src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
@@ -7,12 +7,28 @@ import "./StarRating";
 import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie has {movieRating} stars</p>
+    </>
+  );
+}
 root.render(
   <React.StrictMode>
     {
       /* <App /> */
       <>
-        <StarRating maxRating={5} />
+        <StarRating
+          maxRating={5}
+          messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+          defaultRating={3}
+        />
+        <StarRating size={24} color="red" className="test" defaultRating={3} />
+        <Test />
       </>
     }
   </React.StrictMode>
