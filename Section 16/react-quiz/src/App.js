9b31eb7235fbd7dcae47b8851dbb0fcea90df8be
tushardongre1990 <tushardongre.1990@gmail.com>
2024-01-30 00:17:10 +0530

    191. Loading Questions from a Fake API

--- Section 16/react-quiz/src/App.js
+++ Section 16/react-quiz/src/App.js
@@ -1,7 +1,48 @@
+import { useEffect, useReducer } from "react";
 import DateCounter from "./DateCounter";
 import Header from "./Header";
 import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready','active','finished'
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Unknown Action");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(`HTTP Error Status : ${res.status}`);
+        }
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (error) {
+        dispatch({ type: "dataFailed" });
+      }
+    }
+    fetchQuestions();
+  }, []);
   return (
     <div className="app">
       <Header />
