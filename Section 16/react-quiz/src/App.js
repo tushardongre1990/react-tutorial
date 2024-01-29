93f540b2e6e4a1fe5aa92a61b4b81f3530ac742f
tushardongre1990 <tushardongre.1990@gmail.com>
2024-01-30 01:29:41 +0530

    192. Handling Loading, Error, and Ready Status

--- Section 16/react-quiz/src/App.js
+++ Section 16/react-quiz/src/App.js
@@ -2,6 +2,9 @@ import { useEffect, useReducer } from "react";
 import DateCounter from "./DateCounter";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
 const initialState = {
   questions: [],
@@ -27,7 +30,9 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
   useEffect(function () {
     async function fetchQuestions() {
       try {
@@ -47,8 +52,9 @@ export default function App() {
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
       </Main>
     </div>
   );
---
+++ Section 16/react-quiz/src/StartScreen.js
@@ -0,0 +1,11 @@
+function StartScreen({ numQuestions }) {
+  return (
+    <div className="start">
+      <h2>Welcome to React Quiz!</h2>
+      <h3> {numQuestions} Question to test your react mastery</h3>
+      <button className="btn btn-ui">Let's Start</button>
+    </div>
+  );
+}
+
+export default StartScreen;
