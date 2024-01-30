e12b5a11b3f272aac8393d8d7ad6b5b817187a6d
tushardongre1990 <tushardongre.1990@gmail.com>
2024-01-31 01:00:01 +0530

    194. Displaying Questions

--- Section 16/react-quiz/src/components/App.js
+++ Section 16/react-quiz/src/components/App.js
@@ -11,6 +11,7 @@ const initialState = {
   questions: [],
   // 'loading', 'error', 'ready','active','finished'
   status: "loading",
+  index: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -36,7 +37,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
   useEffect(function () {
@@ -63,7 +67,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
