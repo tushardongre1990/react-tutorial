d41adfba999d0c99f0062f0e2fb885ccaa7110b7
Tushar Dongre <tushardongre.1990@gmail.com>
2024-02-21 01:56:47 +0530

    Route

--- Section 17/worldwise/src/App.jsx
+++ Section 17/worldwise/src/App.jsx
@@ -7,6 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* path -> last part of url(e.g. http://localhost:5173/product) to which components displayed */}
         <Route path="/" element={<Homepage />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
