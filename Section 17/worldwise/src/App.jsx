1e88f3f5c5d332b8849994cac1a915c1c91b600a
Tushar Dongre <tushardongre.1990@gmail.com>
2024-02-22 02:25:28 +0530

    211. Building the App Layout

--- Section 17/worldwise/src/App.jsx
+++ Section 17/worldwise/src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from "./pages/HomePage";
+import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import Product from "./pages/Product";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 
-import AppLayput from "./pages/AppLayput";
+import AppLayput from "./pages/AppLayout";
 function App() {
   return (
     <BrowserRouter>
