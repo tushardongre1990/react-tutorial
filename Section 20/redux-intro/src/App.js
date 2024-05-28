7f82312017ee76e63e829cb536f9ea0b515cf8c0
tushardongre1990 <tushardongre.1990@gmail.com>
2024-05-29 02:09:36 +0530

    268. Dispatching Actions from Our React App

--- Section 20/redux-intro/src/App.js
+++ Section 20/redux-intro/src/App.js
@@ -2,15 +2,23 @@ import CreateCustomer from "./features/customers/CreateCustomer";
 import Customer from "./features/customers/Customer";
 import AccountOperations from "./features/accounts/AccountOperations";
 import BalanceDisplay from "./features/accounts/BalanceDisplay";
+import { useSelector } from "react-redux";
 
 function App() {
+  const fullName = useSelector((state) => state.customer.fullName);
+  console.log(fullName);
   return (
     <div>
       <h1>🏦 The React-Redux Bank ⚛️</h1>
-      <CreateCustomer />
-      <Customer />
-      <AccountOperations />
-      <BalanceDisplay />
+      {fullName === "" ? (
+        <CreateCustomer />
+      ) : (
+        <>
+          <Customer />
+          <AccountOperations />
+          <BalanceDisplay />
+        </>
+      )}
     </div>
   );
 }
