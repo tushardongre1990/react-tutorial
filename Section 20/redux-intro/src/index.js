44a3204a75978a142d7510d11447c8ef8a466965
tushardongre1990 <tushardongre.1990@gmail.com>
2024-05-29 01:09:10 +0530

    267. Back to React! Connecting our Redux App With React

--- Section 20/redux-intro/src/index.js
+++ Section 20/redux-intro/src/index.js
@@ -2,14 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import store from "./store";
+import store from "./store.js";
+import { Provider } from "react-redux";
 
-store.dispatch({ type: "account/deposit", payload: 250 });
-console.log(store.getState());
+// store.dispatch({ type: "account/deposit", payload: 2250 });
+// store.dispatch({
+//   type: "customer/createCustomer",
+//   payload: {
+//     fullName: "Tushar Dongre",
+//     nationalId: "440034",
+//     createdAt: new Date().toISOString(),
+//   },
+// });
+// console.log(store.getState());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
