89caee8b58db7fce9c39872bc300a0adadb1b887
tushardongre1990 <tushardongre.1990@gmail.com>
2024-07-02 02:11:05 +0530

    298. The Box Model: Spacing, Borders, and Display

--- Section 22-23/fast-react-pizza/src/features/user/UserName.jsx
+++ Section 22-23/fast-react-pizza/src/features/user/UserName.jsx
@@ -1,5 +1,5 @@
 function UserName() {
-    return <div className="text-sm font-semibold">Tushar</div>
+    return <div className="hidden text-sm font-semibold">Tushar</div>
 }
 
 export default UserName
