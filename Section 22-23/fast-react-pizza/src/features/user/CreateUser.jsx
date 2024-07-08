7aadeb2a7896fc4d3d346e26df875756c94d12b1
tushardongre1990 <tushardongre.1990@gmail.com>
2024-07-09 01:52:54 +0530

    299. Responsive Design

--- Section 22-23/fast-react-pizza/src/features/user/CreateUser.jsx
+++ Section 22-23/fast-react-pizza/src/features/user/CreateUser.jsx
@@ -1,30 +1,33 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 function CreateUser() {
-  const [username, setUsername] = useState('');
+    const [username, setUsername] = useState('')
 
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
+    function handleSubmit(e) {
+        e.preventDefault()
+    }
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <p>👋 Welcome! Please start by telling us your name:</p>
+    return (
+        <form onSubmit={handleSubmit}>
+            <p className="mb-4 text-sm text-stone-600 md:text-base">
+                👋 Welcome! Please start by telling us your name:
+            </p>
 
-      <input
-        type="text"
-        placeholder="Your full name"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
+            <input
+                className="w-72"
+                type="text"
+                placeholder="Your full name"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+            />
 
-      {username !== '' && (
-        <div>
-          <button>Start ordering</button>
-        </div>
-      )}
-    </form>
-  );
+            {username !== '' && (
+                <div>
+                    <button>Start ordering</button>
+                </div>
+            )}
+        </form>
+    )
 }
 
-export default CreateUser;
+export default CreateUser
