7e405d9632e80bc73490b031d5a9315d9e5bf200
tushardongre1990 <tushardongre.1990@gmail.com>
2024-07-26 02:03:43 +0530

    309. Styling the Cart

--- Section 22-23/fast-react-pizza/src/features/order/CreateOrder.jsx
+++ Section 22-23/fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -1,11 +1,5 @@
 import { useState } from 'react'
-import {
-    Form,
-    redirect,
-    useActionData,
-    useNavigate,
-    useNavigation,
-} from 'react-router-dom'
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom'
 import { createOrder } from '../../services/apiRestaurant'
 import Button from '../../ui/Button'
 
@@ -49,38 +43,45 @@ function CreateOrder() {
     const cart = fakeCart
 
     return (
-        <div>
-            <h2>Ready to order? Lets go!</h2>
+        <div className="px-4 py-6">
+            <h2 className="mb-8 text-xl font-semibold">
+                Ready to order? Lets go!
+            </h2>
 
+            {/* <Form method="POST" action="/order/new"> */}
             <Form method="POST">
-                <div>
-                    <label>First Name</label>
+                <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+                    <label className="sm:basis-40">First Name</label>
                     <input
-                        className="input"
+                        className="input grow"
                         type="text"
                         name="customer"
                         required
                     />
                 </div>
 
-                <div>
-                    <label>Phone number</label>
-                    <div>
+                <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+                    <label className="sm:basis-40">Phone number</label>
+                    <div className="grow">
                         <input
-                            className="input"
+                            className="input w-full"
                             type="tel"
                             name="phone"
                             required
                         />
+                        {formErrors?.phone && (
+                            <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                                {formErrors.phone}
+                            </p>
+                        )}
                     </div>
-                    {formErrors?.phone && <p>{formErrors.phone}</p>}
                 </div>
 
-                <div>
-                    <label>Address</label>
-                    <div>
+                <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+                    <label className="sm:basis-40">Address</label>
+                    <div className="grow">
                         <input
-                            className="input"
+                            className="input w-full"
                             type="text"
                             name="address"
                             required
@@ -88,16 +89,16 @@ function CreateOrder() {
                     </div>
                 </div>
 
-                <div>
+                <div className="mb-12 flex items-center gap-5">
                     <input
+                        className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
                         type="checkbox"
                         name="priority"
                         id="priority"
-                        className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
                         // value={withPriority}
                         // onChange={(e) => setWithPriority(e.target.checked)}
                     />
-                    <label htmlFor="priority">
+                    <label htmlFor="priority" className="font-medium">
                         Want to yo give your order priority?
                     </label>
                 </div>
@@ -109,7 +110,7 @@ function CreateOrder() {
                         value={JSON.stringify(cart)}
                     />
                     <Button disabled={isSubmitting} type="primary">
-                        {isSubmitting ? 'Placing Order' : 'Order now'}
+                        {isSubmitting ? 'Placing order....' : 'Order now'}
                     </Button>
                 </div>
             </Form>
@@ -117,10 +118,10 @@ function CreateOrder() {
     )
 }
 
-// whenever Form is submitted ,react-router wwill call action function and passes the request that was submitted.
 export async function action({ request }) {
     const formData = await request.formData()
     const data = Object.fromEntries(formData)
+
     const order = {
         ...data,
         cart: JSON.parse(data.cart),
@@ -128,16 +129,19 @@ export async function action({ request }) {
     }
 
     const errors = {}
-    if (!isValidPhone(order.phone)) {
-        errors.phone = 'Please enter correct phone number. '
-    }
+    if (!isValidPhone(order.phone))
+        errors.phone =
+            'Please give us your correct phone number. We might need it to contact you.'
+
     if (Object.keys(errors).length > 0) return errors
 
-    // If everyting is okay create new order and redirect
-    const newOrder = await createOrder(order)
-    console.log(newOrder)
+    // If everything is okay, create new order and redirect
 
-    return redirect(`/order/${newOrder.id}`) // useNavigate can't be use here 'cause it works only in hooks
+    // const newOrder = await createOrder(order);
+
+    // return redirect(`/order/${newOrder.id}`);
+
+    return null
 }
 
 export default CreateOrder
