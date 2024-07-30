e261bac84495f68d9b1901781e159dfde71ce3d3
tushardongre1990 <tushardongre.1990@gmail.com>
2024-07-31 01:47:52 +0530

    314. Reading and Updating the User State

--- Section 22-23-24/fast-react-pizza/src/features/order/CreateOrder.jsx
+++ Section 22-23-24/fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom'
 import { createOrder } from '../../services/apiRestaurant'
 import Button from '../../ui/Button'
+import { useSelector } from 'react-redux'
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -41,6 +42,7 @@ function CreateOrder() {
 
     // const [withPriority, setWithPriority] = useState(false);
     const cart = fakeCart
+    const username = useSelector((state) => state.user.username)
 
     return (
         <div className="px-4 py-6">
@@ -57,6 +59,7 @@ function CreateOrder() {
                         type="text"
                         name="customer"
                         required
+                        defaultValue={username}
                     />
                 </div>
 
