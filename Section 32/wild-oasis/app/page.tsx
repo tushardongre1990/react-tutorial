a21a4b59e5cfc3b0ac621ec36035c6448cddf8cd
tushardongre1990 <tushardongre.1990@gmail.com>
2025-05-05 00:26:09 +0530

    424. Creating a Layout

--- Section 32/wild-oasis/app/page.tsx
+++ Section 32/wild-oasis/app/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from "./components/Navigation";
-
 export default function Home() {
   return (
     <div>
-      <Navigation />
       <h1>The Wild Oasis. Welcome to Paradise.</h1>
     </div>
   );
